1f62e1b2420b2dab1447dbf0c26dd958908832ba
Myshkan <39353542+Myshkan@users.noreply.github.com>
2018-08-25 15:41:42 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$('.second-section-slider').slick({
+   arrows: false,
+   dots: true,
+    slidesToShow: 1,
+    adaptiveHeight: true
+});
+
+$('.fourth-slider').slick({
+   arrows: false,
+   dots: true,
+   slidesToShow: 1,
+   adaptiveHeight: true,
+   autoplay: true,
+   autoplaySpeed: 4000
+});
